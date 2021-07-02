413cf2a43fdcbf5105d50effcc3688eda146d8b4
MoSoliman <soliman@email.com>
2021-07-02 15:09:52 +0200

    Initial commit

---
+++ Land of Maniacs/Assets/My Scripts/LightSettingsPlayer.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+
+public class LightSettingsPlayer : MonoBehaviour
+{
+    [SerializeField] PostProcessVolume MyVolume;
+    [SerializeField] PostProcessProfile Standard;
+    [SerializeField] PostProcessProfile NightVision;
+    [SerializeField] GameObject NightVisionOverlay;
+    [SerializeField] GameObject FlashlightObject;
+
+    private bool NightVisionActive = false;
+    private bool FlashlightActive = false;
+
+
+    void Start()
+    {
+        FlashlightObject.gameObject.SetActive(false);
+        NightVisionOverlay.gameObject.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+            if (NightVisionActive == false)
+            {
+                MyVolume.profile = NightVision;
+                NightVisionOverlay.gameObject.SetActive(true);
+                NightVisionActive = true;
+            }
+            else
+            {
+                MyVolume.profile = Standard;
+                NightVisionOverlay.gameObject.SetActive(false);
+                NightVisionActive = false;
+            }
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            if (FlashlightActive == false)
+            {
+                FlashlightObject.gameObject.SetActive(true);
+                FlashlightActive = true;
+            }
+            else
+            {
+                FlashlightObject.gameObject.SetActive(false);
+                FlashlightActive = false;
+            }
+
+        }
+
+
+
+    }
+}
