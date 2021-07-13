b5c298ded4d8cc9a4db9f00ed8cace9e2534600d
MoSoliman <soliman@email.com>
2021-07-13 23:51:12 +0200

    Added One Time Sounds Prefab
    Created a prefab to play sounds only one time through a simple script. It is used when we want to play a sound when a certain event happens or the user enters or leaves a specific area ..etc. However, there's a SerializeField that could disable the one time playback effect and make the sound play every time the player collides with the sound source (There's a pause time between every playback that can also be set by preference from the inspector).

--- Land of Maniacs/Assets/My Scripts/OneTimeSound.cs
+++ Land of Maniacs/Assets/My Scripts/OneTimeSound.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class OneTimeSound : MonoBehaviour
 {
+    private AudioSource OneTime;
+    private Collider Collider;
+    [SerializeField] bool isOneTime = false;
+    [SerializeField] float PauseTime = 5.0f;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        OneTime = GetComponent<AudioSource>();
+        Collider = GetComponent<Collider>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void OnTriggerEnter(Collider other)
+    { 
+    if(other.CompareTag("Player"))
+        {
+            OneTime.Play();
+            Collider.enabled = false;
+
+            if(isOneTime == false)
+            {
+                StartCoroutine(Reset());
+            }
+        }
+    else
+        {
+            Destroy(gameObject, PauseTime);
+        }
+
+    IEnumerator Reset()
+        {
+            yield return new WaitForSeconds(PauseTime);
+            Collider.enabled = true;
+        }
     }
+
 }
