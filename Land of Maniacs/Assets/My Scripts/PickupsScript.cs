d51f2640b47c74e74b37f1c2a6243360ff5e4204
MoSoliman <soliman@email.com>
2021-07-17 02:53:43 +0200

    Imported and Animated Weapons
    Weapons:
    -Axe
    -Knife
    -Baseball Bat
    -M1911A Handgun
    -Crossbow
    
    -Added these weapons prefabs and animated them to glow in the dark
    -Added pickup functionality to these weapons and made the limit 1 pickup per weapon.

--- Land of Maniacs/Assets/My Scripts/PickupsScript.cs
+++ Land of Maniacs/Assets/My Scripts/PickupsScript.cs
@@ -8,14 +8,32 @@ public class PickupsScript : MonoBehaviour
 
     RaycastHit hit;
     [SerializeField] float Distance = 4.0f;
+
     [SerializeField] GameObject PickupMSG;
+    [SerializeField] GameObject PickupAxeMSG;
+    [SerializeField] GameObject PickupKnifeMSG;
+    [SerializeField] GameObject PickupBaseballBatMSG;
+    [SerializeField] GameObject PickupHandgunMSG;
+    [SerializeField] GameObject PickupCrossbowMSG;
     [SerializeField] GameObject BatteriesFullMSG;
     [SerializeField] GameObject ApplesFullMSG;
+    [SerializeField] GameObject HasWeaponMSG;
+
     private AudioSource MyPlayer;
+
     private float RayDistance;
+
     private bool CanSeePickup = false;
+    private bool CanSeeAxe = false;
+    private bool CanSeeKnife = false;
+    private bool CanSeeBaseballBat = false;
+    private bool CanSeeHandgun = false;
+    private bool CanSeeCrossbow = false;
+
     private bool BatteriesFull = false;
     private bool ApplesFull = false;
+    private bool HasWeapon = false;
+
 
 
     // Start is called before the first frame update
@@ -26,9 +44,14 @@ public class PickupsScript : MonoBehaviour
         PickupMSG.gameObject.SetActive(false);
         BatteriesFullMSG.gameObject.SetActive(false);
         ApplesFullMSG.gameObject.SetActive(false);
+        HasWeaponMSG.gameObject.SetActive(false);
+        PickupAxeMSG.gameObject.SetActive(false);
+        PickupKnifeMSG.gameObject.SetActive(false);
+        PickupBaseballBatMSG.gameObject.SetActive(false);
+        PickupHandgunMSG.gameObject.SetActive(false);
+        BatteriesFullMSG.gameObject.SetActive(false);
         RayDistance = Distance;
     }
-    public float timetowait = 2f;
     // Update is called once per frame
     void Update()
     {
@@ -69,10 +92,100 @@ public class PickupsScript : MonoBehaviour
 
                 }
             }
+            else if (hit.transform.tag == "Axe")
+            {
+                CanSeeAxe = true;
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    if (SaveScript.Axe == true)
+                    {
+                        HasWeapon = true;
+                    }
+                    else
+                    {
+                        Destroy(hit.transform.gameObject);
+                        SaveScript.Axe = true;
+                        MyPlayer.Play();
+                    }
+                }
+            }
+            else if (hit.transform.tag == "Knife")
+            {
+                CanSeeKnife = true;
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    if (SaveScript.Knife == true)
+                    {
+                        HasWeapon = true;
+                    }
+                    else
+                    {
+                        Destroy(hit.transform.gameObject);
+                        SaveScript.Knife = true;
+                        MyPlayer.Play();
+                    }
+                }
+            }
+            else if (hit.transform.tag == "BaseballBat")
+            {
+                CanSeeBaseballBat = true;
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    if (SaveScript.BaseballBat == true)
+                    {
+                        HasWeapon = true;
+                    }
+                    else
+                    {
+                        Destroy(hit.transform.gameObject);
+                        SaveScript.BaseballBat = true;
+                        MyPlayer.Play();
+                    }
+                }
+            }
+            else if (hit.transform.tag == "Crossbow")
+            {
+                CanSeeCrossbow = true;
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    if (SaveScript.Crossbow == true)
+                    {
+                        HasWeapon = true;
+                    }
+                    else
+                    {
+                        Destroy(hit.transform.gameObject);
+                        SaveScript.Crossbow = true;
+                        MyPlayer.Play();
+                    }
+                }
+            }
+            else if (hit.transform.tag == "Handgun")
+            {
+                CanSeeHandgun = true;
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    if (SaveScript.Handgun == true)
+                    {
+                        HasWeapon = true;
+                    }
+                    else
+                    {
+                        Destroy(hit.transform.gameObject);
+                        SaveScript.Handgun = true;
+                        MyPlayer.Play();
+                    }
+                }
+            }
             else
             {
                 CanSeePickup = false;
-            }
+                CanSeeAxe = false;
+                CanSeeKnife = false;
+                CanSeeBaseballBat = false;
+                CanSeeHandgun = false;
+                CanSeeCrossbow = false;
+}
         }
 
         if (CanSeePickup == true)
@@ -83,6 +196,65 @@ public class PickupsScript : MonoBehaviour
         if (CanSeePickup == false)
         {
             PickupMSG.gameObject.SetActive(false);
+            RayDistance = Distance;
+
+        }
+
+        if (CanSeeAxe == true)
+        {
+            PickupAxeMSG.gameObject.SetActive(true);
+            RayDistance = 10000f;
+        }
+        if (CanSeeAxe == false)
+        {
+            PickupAxeMSG.gameObject.SetActive(false);
+            RayDistance = Distance;
+
+        }
+
+        if (CanSeeKnife == true)
+        {
+            PickupKnifeMSG.gameObject.SetActive(true);
+            RayDistance = 10000f;
+        }
+        if (CanSeeKnife == false)
+        {
+            PickupKnifeMSG.gameObject.SetActive(false);
+            RayDistance = Distance;
+
+        }
+
+        if (CanSeeBaseballBat == true)
+        {
+            PickupBaseballBatMSG.gameObject.SetActive(true);
+            RayDistance = 10000f;
+        }
+        if (CanSeeBaseballBat == false)
+        {
+            PickupBaseballBatMSG.gameObject.SetActive(false);
+            RayDistance = Distance;
+
+        }
+        if (CanSeeHandgun == true)
+        {
+            PickupHandgunMSG.gameObject.SetActive(true);
+            RayDistance = 10000f;
+        }
+        if (CanSeeHandgun == false)
+        {
+            PickupHandgunMSG.gameObject.SetActive(false);
+            RayDistance = Distance;
+
+        }
+
+        if (CanSeeCrossbow == true)
+        {
+            PickupCrossbowMSG.gameObject.SetActive(true);
+            RayDistance = 10000f;
+        }
+        if (CanSeeCrossbow == false)
+        {
+            PickupCrossbowMSG.gameObject.SetActive(false);
             RayDistance = Distance;
 
         }
@@ -101,11 +273,24 @@ public class PickupsScript : MonoBehaviour
             StartCoroutine(ApplesReset());
         }
 
-
+        if (HasWeapon == true)
+        {
+            HasWeaponMSG.gameObject.SetActive(true);
+            PickupMSG.gameObject.SetActive(false);
+            StartCoroutine(WeaponReset());
+        }
 
     }
 
-   IEnumerator BatteriesReset()
+    IEnumerator WeaponReset()
+    {
+        yield return new WaitForSeconds(2);
+        HasWeaponMSG.gameObject.SetActive(false);
+        HasWeapon = false;
+
+    }
+
+    IEnumerator BatteriesReset()
    {
         yield return new WaitForSeconds(2);
         BatteriesFullMSG.gameObject.SetActive(false);
--- Land of Maniacs/Assets/My Scripts/SaveScript.cs
+++ Land of Maniacs/Assets/My Scripts/SaveScript.cs
@@ -12,4 +12,10 @@ public class SaveScript : MonoBehaviour
     public static bool NVLightOn = false;
     public static int Apples = 0;
     public static int Batteries = 0;
+    public static bool Axe = false;
+    public static bool Knife = false;
+    public static bool BaseballBat = false;
+    public static bool Handgun = false;
+    public static bool Crossbow = false;
+
 }
