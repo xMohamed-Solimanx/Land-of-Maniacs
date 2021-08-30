ef828f56bd3a5caffa20bdffb72bb17950c04be8
MoSoliman <soliman@email.com>
2021-08-30 17:32:30 +0200

    Added Door Sounds
    -Now, different door sounds will be played when the player opens/closes the doors.
    -Readjusted the door rotator prefab to contain the audio source component.

--- Land of Maniacs/Assets/My Scripts/DoorScript.cs
+++ Land of Maniacs/Assets/My Scripts/DoorScript.cs
@@ -7,10 +7,21 @@ public class DoorScript : MonoBehaviour
 
     private Animator Anim;
     public bool IsOpen = false;
+    private AudioSource MyPlayer;
+    [SerializeField] AudioClip CabinSound;
+    [SerializeField] AudioClip RoomSound;
+    [SerializeField] AudioClip HouseSound;
+    [SerializeField] bool Cabin;
+    [SerializeField] bool Room;
+    [SerializeField] bool House;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
         Anim = GetComponent<Animator>();
+        MyPlayer = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -26,12 +37,42 @@ public class DoorScript : MonoBehaviour
         {
             Anim.SetTrigger("Open");
             IsOpen = true;
+            if (Cabin)
+            {
+                MyPlayer.clip = CabinSound;
+                MyPlayer.Play();
+            }
+            if (Room)
+            {
+                MyPlayer.clip = RoomSound;
+                MyPlayer.Play();
+            }
+            if (House)
+            {
+                MyPlayer.clip = HouseSound;
+                MyPlayer.Play();
+            }
         }
 
         else if (IsOpen)
         {
             Anim.SetTrigger("Close");
             IsOpen = false;
+            if (Cabin)
+            {
+                MyPlayer.clip = CabinSound;
+                MyPlayer.Play();
+            }
+            if (Room)
+            {
+                MyPlayer.clip = RoomSound;
+                MyPlayer.Play();
+            }
+            if (House)
+            {
+                MyPlayer.clip = HouseSound;
+                MyPlayer.Play();
+            }
         }
     }
 }
