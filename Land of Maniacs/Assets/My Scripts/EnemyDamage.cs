9005e2a0aff64a8fed31757a6844401ed2382ad6
xMohamed-Solimanx <soliman@email.com>
2021-09-21 12:27:29 +0200

    Added Enemy Death Animation
    -Now, if the enemy's health is below or equal to zero, the death animation plays and the enemy object is destroyed after sometime.
    -Added simultaneous sounds when attacking enemies.

--- Land of Maniacs/Assets/My Scripts/EnemyDamage.cs
+++ Land of Maniacs/Assets/My Scripts/EnemyDamage.cs
@@ -4,26 +4,43 @@ using UnityEngine;
 
 public class EnemyDamage : MonoBehaviour
 {
-    public int EnemyHealth = 100;
+    [SerializeField] int EnemyHealth = 100;
     private AudioSource MyPlayer;
+    [SerializeField] AudioSource StabPlayer;
+    private Animator Anim;
+
+    [SerializeField] GameObject EnemyObject;
+
+    private bool HasDied = false; //used in Update() so that the death animation doesn't loop
     // Start is called before the first frame update
     void Start()
     {
         MyPlayer = GetComponent<AudioSource>();
+        Anim = GetComponentInParent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(EnemyHealth <= 0)
+        {
+            if(HasDied == false)
+            {
+                Anim.SetTrigger("Death");
+                HasDied = true;
+
+                Destroy(EnemyObject, 20f);
+            }
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Knife"))
         {
-            EnemyHealth -= 50;
+            EnemyHealth -= 25;
             MyPlayer.Play();
+            StabPlayer.Play();
         }
     }
 }
