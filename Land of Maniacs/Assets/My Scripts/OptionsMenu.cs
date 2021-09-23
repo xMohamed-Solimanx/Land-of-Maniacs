8ae19cbc110cb975c06132c63a2e9ba9078c2da8
xMohamed-Solimanx <soliman@email.com>
2021-09-23 23:38:36 +0200

    Added Visual, Sound, and Controls Options Layout
    Created the text, buttons, sliders, and checkboxes for the various options of the Visual, Sound, and Controls settings. The rest is yet to be created.

---
+++ Land of Maniacs/Assets/My Scripts/OptionsMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OptionsMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = true;
+        Time.timeScale = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Cursor.visible = true;
+    }
+}
