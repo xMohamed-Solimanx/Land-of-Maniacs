46ca2304b50f25a5d78220d4867850ba8286f48f
xMohamed-Solimanx <soliman@email.com>
2021-09-24 10:56:48 +0200

    Created the rest of the options UI layout
    -Created the UI layout of "Difficulty", "Save", and "Back to Menu" options.
    
    -Created a script to navigate through each option.

--- Land of Maniacs/Assets/My Scripts/OptionsMenu.cs
+++ Land of Maniacs/Assets/My Scripts/OptionsMenu.cs
@@ -4,11 +4,27 @@ using UnityEngine;
 
 public class OptionsMenu : MonoBehaviour
 {
+    [SerializeField] GameObject VisualsPanel;
+    [SerializeField] GameObject SoundsPanel;
+    [SerializeField] GameObject ControlsPanel;
+    [SerializeField] GameObject DifficultyPanel;
+    [SerializeField] GameObject SavePanel;
+    [SerializeField] GameObject BackToMenuPanel;
+
     // Start is called before the first frame update
     void Start()
     {
         Cursor.visible = true;
         Time.timeScale = 0;
+
+        VisualsPanel.gameObject.SetActive(true);
+        SoundsPanel.gameObject.SetActive(false);
+        ControlsPanel.gameObject.SetActive(false);
+        DifficultyPanel.gameObject.SetActive(false);
+        SavePanel.gameObject.SetActive(false);
+        BackToMenuPanel.gameObject.SetActive(false);
+
+
     }
 
     // Update is called once per frame
@@ -16,4 +32,67 @@ public class OptionsMenu : MonoBehaviour
     {
         Cursor.visible = true;
     }
+
+
+
+    public void Visuals()
+    {
+        VisualsPanel.gameObject.SetActive(true);
+        SoundsPanel.gameObject.SetActive(false);
+        ControlsPanel.gameObject.SetActive(false);
+        DifficultyPanel.gameObject.SetActive(false);
+        SavePanel.gameObject.SetActive(false);
+        BackToMenuPanel.gameObject.SetActive(false);
+    }
+
+    public void Sounds()
+    {
+        VisualsPanel.gameObject.SetActive(false);
+        SoundsPanel.gameObject.SetActive(true);
+        ControlsPanel.gameObject.SetActive(false);
+        DifficultyPanel.gameObject.SetActive(false);
+        SavePanel.gameObject.SetActive(false);
+        BackToMenuPanel.gameObject.SetActive(false);
+    }
+
+    public void Controls()
+    {
+        VisualsPanel.gameObject.SetActive(false);
+        SoundsPanel.gameObject.SetActive(false);
+        ControlsPanel.gameObject.SetActive(true);
+        DifficultyPanel.gameObject.SetActive(false);
+        SavePanel.gameObject.SetActive(false);
+        BackToMenuPanel.gameObject.SetActive(false);
+    }
+
+    public void Difficulty()
+    {
+        VisualsPanel.gameObject.SetActive(false);
+        SoundsPanel.gameObject.SetActive(false);
+        ControlsPanel.gameObject.SetActive(false);
+        DifficultyPanel.gameObject.SetActive(true);
+        SavePanel.gameObject.SetActive(false);
+        BackToMenuPanel.gameObject.SetActive(false);
+    }
+
+    public void Save()
+    {
+        VisualsPanel.gameObject.SetActive(false);
+        SoundsPanel.gameObject.SetActive(false);
+        ControlsPanel.gameObject.SetActive(false);
+        DifficultyPanel.gameObject.SetActive(false);
+        SavePanel.gameObject.SetActive(true);
+        BackToMenuPanel.gameObject.SetActive(false);
+    }
+
+    public void BackToMenu()
+    {
+        VisualsPanel.gameObject.SetActive(false);
+        SoundsPanel.gameObject.SetActive(false);
+        ControlsPanel.gameObject.SetActive(false);
+        DifficultyPanel.gameObject.SetActive(false);
+        SavePanel.gameObject.SetActive(false);
+        BackToMenuPanel.gameObject.SetActive(true);
+    }
 }
+
